0c021de2c5e8d3fac4586af2b21b45895a5f26fb
Charli <cm32brown@gmail.com>
2022-05-13 16:50:17 -0600

    Create Boot MVC project, add JPA project dependencies

---
+++ FlightLogApp/src/main/java/com/skilldistillery/flightlog/FlightLogAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.flightlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightLogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightLogAppApplication.class, args);
+	}
+
+}
---
+++ FlightLogApp/src/test/java/com/skilldistillery/flightlog/FlightLogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.flightlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightLogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
